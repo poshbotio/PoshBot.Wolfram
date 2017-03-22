649e6214ef52fb676bf9a48e9dc59adfcacb66d4
Brandon Olin <brandon@devblackops.io>
2017-03-21 21:45:51 -0700

    Add module

---
+++ PoshBot.Wolfram/PoshBot.Wolfram.psm1
@@ -0,0 +1,23 @@
+
+function AskWolfram {
+    <#
+    .SYNOPSIS
+        Asks WolframAlpha a question
+    .EXAMPLE
+        !askwolfram '34th president of the united states'
+    #>
+    [cmdletbinding()]
+    param(
+        [PoshBot.FromConfig('ApiKey')]
+        [parameter(Mandatory)]
+        [string]$ApiKey,
+
+        [parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$Arguments
+    )
+
+    $q = $Arguments -join ' '
+    $url = "http://api.wolframalpha.com/v1/result?i=$q&appid=$ApiKey"
+    $r = Invoke-RestMethod -Uri $url
+    Write-Output $r
+}
